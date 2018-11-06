ad9ead0f11046ba63d090a9ab8814a68f40b1555
xiaodabao <xiaojin1992@126.com>
2018-11-06 22:04:16 +0800

    ..

---
+++ config/config.default.js
@@ -0,0 +1,13 @@
+exports.keys = 'i love you';
+
+exports.view = {
+  defaultViewEngine: 'nunjucks',
+  mapping: {
+    '.tpl': 'nunjucks',
+  },
+};
+
+exports.news = {
+  pageSize: 5,
+  serverUrl: 'https://hacker-news.firebaseio.com/v0',
+};
