105c5c2d122e5a84fbf8baea7017bcee4722a83a
xiaodabao <xiaojin1992@126.com>
2018-11-14 09:37:24 +0800

    终究会坠落

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,7 +2,7 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = `here is ${this.app.config.env}`;
+    this.ctx.body = `here is ${this.app.config.env} ${this.ctx.query.q} ${this.ctx.params.id}`;
   }
 }
 
